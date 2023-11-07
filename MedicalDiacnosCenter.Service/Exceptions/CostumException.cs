2c56f6b562f5f3145bdcd5474a49e7a372b250a4
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-11-07 22:42:11 +0500

    Exceptions added

---
+++ MedicalDiacnosCenter.Service/Exceptions/CostumException.cs
@@ -0,0 +1,11 @@
+﻿namespace MedicalDiacnosCenter.Service.Exceptions;
+
+public class CostumException : Exception
+{
+    public int StatusCode { get; set; }
+
+    public CostumException(int code, string message) : base(message)
+    {
+        StatusCode = code;
+    }
+}
